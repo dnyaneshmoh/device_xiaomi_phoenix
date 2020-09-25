d58680cae18df2caa842a72b6e443a9c49fcff10
Dnyanesh <dmohite@gmail.com>
2020-09-25 19:06:32 +0530

    phoenix: Zenx-OS Bringups

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -1,12 +1,12 @@
 #
-# Copyright (C) 2020 The LineageOS Project
+# Copyright (C) 2020 The Zenx-OS Project
 #
 # SPDX-License-Identifier: Apache-2.0
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/lineage_phoenix.mk
+    $(LOCAL_DIR)/zenx_phoenix.mk
 
 COMMON_LUNCH_CHOICES := \
-    lineage_phoenix-userdebug \
-    lineage_phoenix-eng
+    zenx_phoenix-userdebug \
+    zenx_phoenix-eng
--- zenx_phoenix.mk
+++ zenx_phoenix.mk
@@ -1,20 +1,28 @@
 #
-# Copyright (C) 2020 The LineageOS Project
+# Copyright (C) 2020 The ZenX-OS Project
 #
 # SPDX-License-Identifier: Apache-2.0
 #
 
 $(call inherit-product, device/xiaomi/phoenix/device.mk)
 
-# Inherit some common Lineage stuff.
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+# Inherit some common ZenX-OS stuff.
+$(call inherit-product, vendor/zenx/config/common_full_phone.mk)
 
 # Device identifier. This must come after all inclusions.
-PRODUCT_NAME := lineage_phoenix
+PRODUCT_NAME := zenx_phoenix
 PRODUCT_DEVICE := phoenix
 PRODUCT_BRAND := Redmi
 PRODUCT_MODEL := Redmi K30
 PRODUCT_MANUFACTURER := Xiaomi
+
+# Boot animation
+TARGET_SCREEN_HEIGHT := 2400
+TARGET_SCREEN_WIDTH := 1080
+TARGET_BOOT_ANIMATION_RES := 1080
+
+# Inherit GApps
+TARGET_GAPPS_ARCH := arm64
 
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
 
