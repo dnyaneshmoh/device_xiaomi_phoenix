679ff5d983d62b86808f89d0529b08a264ff4290
Dnyanesh <dmohite@gmail.com>
2021-02-14 13:38:46 +0530

    phoenix: PixelPlusUI Bringups

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -1,12 +1,12 @@
 #
-# Copyright (C) 2020 The LineageOS Project
+# Copyright (C) 2020 The aospOS Project
 #
 # SPDX-License-Identifier: Apache-2.0
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/lineage_phoenix.mk
+    $(LOCAL_DIR)/aosp_phoenix.mk
 
 COMMON_LUNCH_CHOICES := \
-    lineage_phoenix-userdebug \
-    lineage_phoenix-eng
+    aosp_phoenix-userdebug \
+    aosp_phoenix-eng
